38d2a36a8e1da8c1d83129b8930a9e6ce5e9a18c
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-03-02 16:48:34 +0100

    JS

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,43 @@
 "use strict"
 
 
+const board = document.querySelector('.board');
+const select = document.querySelector('.selection');
+const playBtn = document.querySelector('.play');
 
-function createCell(htmlElement, className){
-    const element = document.createElement(htmlElement);
-    element.classList.add(className);
+let numRows = 10;
+let numCols = 10;
 
-    return element;
+function generateGrid() {
+  
+    board.innerHTML = '';
+  const difficulty = select.value;
+  if (difficulty === '1') {
+    numRows = 10;
+    numCols = 10;
+  } else if (difficulty === '2') {
+    numRows = 9;
+    numCols = 9;
+  } else if (difficulty === '3') {
+    numRows = 7;
+    numCols = 7;
+  }
+
+  // Generate the grid
+  for (let row = 0; row < numRows; row++) {
+    for (let col = 0; col < numCols; col++) {
+      const cell = document.createElement('div');
+      cell.classList.add(`cell-${difficulty}`);
+      cell.textContent = row * numCols + col + 1;
+      cell.addEventListener('click', handleCellClick);
+      board.appendChild(cell);
+    }
+  }
 }
 
-
-function myAppendElement (containerElement, htmlElement){
-    containerElement.append(htmlElement);
+function handleCellClick(event) {
+  event.target.style.backgroundColor = 'blue';
+  console.log(event.target.textContent);
 }
 
-const containerBoard = document.querySelector(".board");
-createCell("div", "cell");
+playBtn.addEventListener('click', generateGrid);
